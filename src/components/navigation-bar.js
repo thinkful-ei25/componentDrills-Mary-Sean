05bd7b66371a550748163789126c09ce161828e4
Sean Nealon <signalflowsean@gmail.com>
2018-11-05 13:52:00 -0500

    yay its working

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -5,17 +5,19 @@ import './navigation-bar.css';
 // The NavigationBar component goes here.  It should be the default export.
 export default function NavigationBar(props) { 
   console.log(props.links[0].text);
-  const formattedLinks = props.links.map((link, index) => 
-    (<li key={index} >
+  const formattedLinks = props.links.map((link, index) => (
+    <li key={index} >
       <a href={link.href}> {link.text}</a>
-      </li>)
-  ); 
+    </li>
+  )); 
 
   console.log('links', formattedLinks);
   return (
-    <nav className="navigation-bar navigation-bar-nav">
-      <h1 ><strong>{props.title}</strong></h1> 
-      <ul className="">{formattedLinks}</ul>
-      
-    </nav>);  
+    <div className="navigation-bar">
+      <h1 ><strong>{props.title}</strong></h1>
+      <nav className="navigation-bar-nav ">
+       
+        <ul>{formattedLinks}</ul>
+      </nav>
+    </div>);  
 }
