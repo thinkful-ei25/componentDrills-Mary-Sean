1dc98ca6f70816c31013ed38b19e1595ac76f5c9
Sean Nealon <signalflowsean@gmail.com>
2018-11-05 12:37:44 -0500

    title working

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -3,4 +3,17 @@ import React from 'react';
 import './navigation-bar.css';
 
 // The NavigationBar component goes here.  It should be the default export.
+export default function NavigationBar(props) { 
+  const formattedLinks = props.links.map(link => (
+    <a href={props.links.href}> {props.links.title}</a>
+  )); 
 
+  console.log('props:', props.links);
+  return (
+    <div>
+      <p><strong>{props.title}</strong>  
+      <a href={props.links.href}> {props.links.title}</a>
+      </p> 
+      
+    </div>);  
+}
