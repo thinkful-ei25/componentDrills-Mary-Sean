4feb6979320a43e36720c58f61b7c62b66c6fea2
Sean Nealon <signalflowsean@gmail.com>
2018-11-05 12:58:53 -0500

    close enough

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -6,16 +6,16 @@ import './navigation-bar.css';
 export default function NavigationBar(props) { 
   console.log(props.links[0].text);
   const formattedLinks = props.links.map((link, index) => 
-    (<li key={index}>
+    (<li key={index} >
       <a href={link.href}> {link.text}</a>
       </li>)
   ); 
 
   console.log('links', formattedLinks);
   return (
-    <div>
-      <p><strong>{props.title}</strong></p> 
-      <ul>{formattedLinks}</ul>
+    <nav className="navigation-bar navigation-bar-nav">
+      <h1 ><strong>{props.title}</strong></h1> 
+      <ul className="">{formattedLinks}</ul>
       
-    </div>);  
+    </nav>);  
 }
